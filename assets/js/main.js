2fd7e1337258811a456e431fb3d889c460f0e05c
Ricardo <ry.yrupailla@gmail.com>
2019-05-16 17:40:03 -0500

    Add create task function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // this is our initial data
 // the format of the date is yyyy-mm-dd
 
-tasksArray = [
+const tasksArray = [
   {
     description: "Study CSS",
     creation_date: new Date("2019-01-01"),
@@ -21,5 +21,17 @@ tasksArray = [
     marked: false
   }
 ];
+
+function createTask(description, due_date) {
+  newTask = {
+    description: description,
+    creation_date: new Date(),
+    due_date: due_date,
+    marked: false
+  };
+  tasksArray.push(newTask);
+}
+
+createTask("Hola", new Date());
 
 console.log(tasksArray);
